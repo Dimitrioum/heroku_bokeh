f80b7ae15389e18183f67672e325e65e2796ed87
user <malovdmitrij@gmail.com>
2020-04-15 17:15:43 +0300

    add level sensor

--- bokeh_plot.py
+++ bokeh_plot.py
@@ -8,7 +8,7 @@ from bokeh.layouts import row, column
 from bokeh.palettes import Category20
 from bokeh.models.annotations import Title, Legend
 from bokeh.models import LinearAxis, Range1d
-Category10 = Category20[14]
+Category10 = Category20[20]
 from bokeh.plotting import reset_output
 reset_output()
 output_notebook()
@@ -23,7 +23,8 @@ bv2['время формирования точки на БВ'] = pd.to_datetime
 bv2['время прихода точки на сервере'] = pd.to_datetime(bv2['время прихода точки на сервере'])
 
 # df = bv1
-df = bv2
+df = bv2[(['Cекция №1 Уровень НП'] < df['Cекция №1 Уровень НП'].quantile(.95))
+           & (['Cекция №2 Уровень НП'] < df['Cекция №2 Уровень НП'].quantile(.95))]
 
 df['время формирования точки на БВ'] = pd.to_datetime(df['время формирования точки на БВ'], format='%d/%m/%Y')
 
@@ -35,20 +36,27 @@ bline = p1.circle(df['время формирования точки на БВ']
                  y_range_name="binary")
 cline = p1.circle(df['время формирования точки на БВ'], df['Секция №1 Датчик в сливной магистрали'], line_width=2, color=Category10[2],
                  y_range_name="binary")
-dline = p1.circle(df['время формирования точки на БВ'], df[df['Cекция №1 Уровень НП'] < df['Cекция №1 Уровень НП'].quantile(.95)]['Cекция №1 Уровень НП'], line_width=2, color=Category10[3])
-
-eline = p1.circle(df['время формирования точки на БВ'], df['Cекция №3 Заливная горловина'], line_width=2, color=Category10[4],
+ccline = p1.circle(df['время формирования точки на БВ'], df['Секция №1 Датчик на уровне планки'], line_width=2, color=Category10[12],
                  y_range_name="binary")
-fline = p1.circle(df['время формирования точки на БВ'], df['Секция №3 Датчик на дне отсека'], line_width=2, color=Category10[5],
+dline = p1.circle(df['время формирования точки на БВ'], df['Cекция №1 Уровень НП'], line_width=2, color=Category10[3])
+ddline = p1.circle(df['время формирования точки на БВ'], df['Секция №2 Датчик на уровне планки'], line_width=2, color=Category10[4],
                  y_range_name="binary")
-gline = p1.circle(df['время формирования точки на БВ'], df['Секция №3 Датчик в сливной магистрали'], line_width=2, color=Category10[6],
+eline = p1.circle(df['время формирования точки на БВ'], df['Cекция №3 Заливная горловина'], line_width=2, color=Category10[5],
                  y_range_name="binary")
-hline = p1.circle(df['время формирования точки на БВ'], df[df['Cекция №2 Уровень НП'] < df['Cекция №2 Уровень НП'].quantile(.95)]['Cекция №2 Уровень НП'], line_width=2, color=Category10[7])
+fline = p1.circle(df['время формирования точки на БВ'], df['Секция №3 Датчик на дне отсека'], line_width=2, color=Category10[6],
+                 y_range_name="binary")
+gline = p1.circle(df['время формирования точки на БВ'], df['Секция №3 Датчик в сливной магистрали'], line_width=2, color=Category10[13],
+                 y_range_name="binary")
+ggline = p1.circle(df['время формирования точки на БВ'], df['Секция №2 Датчик на уровне планки'], line_width=2, color=Category10[15],
+                 y_range_name="binary")
+hline = p1.circle(df['время формирования точки на БВ'], df['Cекция №2 Уровень НП'], line_width=2, color=Category10[7])
 iline = p1.circle(df['время формирования точки на БВ'], df['Cекция №4 Заливная горловина'], line_width=2, color=Category10[8],
                  y_range_name="binary")
 jline = p1.circle(df['время формирования точки на БВ'], df['Cекция №4 Датчик на дне отсека'], line_width=2, color=Category10[9],
                  y_range_name="binary")
 kline = p1.circle(df['время формирования точки на БВ'], df['Секция №4 Датчик в сливной магистрали'], line_width=2, color=Category10[10])
+kkline = p1.circle(df['время формирования точки на БВ'], df['Секция №4 Датчик на уровне планки'], line_width=2, color=Category10[11],
+                 y_range_name="binary")
 # p2 = figure(x_axis_type='datetime', plot_width=10000)
 # eline = p1.circle(df['время прихода точки на сервере'], df['Скорость'], line_width=2, color=Viridis6[5])
 
@@ -61,14 +69,18 @@ legend = Legend(items=[
     ("Секция №1 Заливная горловина", [aline]),
     ("Секция №1 Датчик на дне отсека", [bline]),
     ("Секция №1 Датчик в сливной магистрали", [cline]),
+    ("Секция №1 Датчик на уровне планки", [ccline]),
     ("Секция №1 Уровень НП", [dline]),
+    ("Секция №2 Датчик на уровне планки", [ddline]),
     ("Cекция №3 Заливная горловина", [eline]),
     ("Секция №3 Датчик на дне отсека", [fline]),
     ("Секция №3 Датчик в сливной магистрали", [gline]),
+    ("Секция №3 Датчик на уровне планки", [ggline]),
     ("Секция №3 Уровень НП", [hline]),
     ("Cекция №4 Заливная горловина", [iline]),
     ("Секция №4 Датчик на дне отсека", [jline]),
     ("Секция №4 Датчик в сливной магистрали", [kline]),
+    ("Секция №4 Датчик на уровне планки", [kkline]),
 ], location=(0, 250))
 
 t = Title()
@@ -79,50 +91,61 @@ p1.add_layout(legend, 'left')
 p1.add_layout(LinearAxis(y_range_name="binary"), 'right')
 # p2.add_layout(legend, 'left')
 checkboxes = CheckboxGroup(labels=list(['Секция №1 Заливная горловина', 'Секция №1 Датчик на дне отсека',
-                                        'Секция №1 Датчик в сливной магистрали', 'Секция №1 Уровень НП',
+                                        'Секция №1 Датчик в сливной магистрали', "Секция №1 Датчик на уровне планки",
+                                        'Секция №1 Уровень НП', "Секция №2 Датчик на уровне планки",
                                         'Cекция №3 Заливная горловина', 'Секция №3 Датчик на дне отсека',
-                                        'Секция №3 Датчик в сливной магистрали', 'Секция №3 Уровень НП',
+                                        'Секция №3 Датчик в сливной магистрали', "Секция №1 Датчик на уровне планки",
+                                        'Секция №3 Уровень НП',
                                         'Cекция №4 Заливная горловина', 'Секция №4 Датчик на дне отсека',
-                                        'Секция №4 Датчик в сливной магистрали',]),
+                                        'Секция №4 Датчик в сливной магистрали', "Секция №4 Датчик на уровне планки"]),
                            active=[0])
 callback = CustomJS(code="""aline.visible = false; // aline and etc.. are 
                             bline.visible = false; // passed in from args
-                            cline.visible = false; 
+                            cline.visible = false;
+                            ccline.visible = false;
                             dline.visible = false;
+                            ddline.visible = false;
                             eline.visible = false;
                             fline.visible = false; 
                             gline.visible = false;
+                            ggline.visible = false;
                             hline.visible = false;
                             iline.visible = false; 
                             jline.visible = false;
                             kline.visible = false;
+                            kkline.visible = false;
                             // cb_obj is injected in thanks to the callback
                             if (cb_obj.active.includes(0)){aline.visible = true;} 
                                 // 0 index box is aline
                             if (cb_obj.active.includes(1)){bline.visible = true;} 
                                 // 1 index box is bline
                             if (cb_obj.active.includes(2)){cline.visible = true;} 
+                             if (cb_obj.active.includes(3)){ccline.visible = true;} 
                                 // 1 index box is bline
-                            if (cb_obj.active.includes(3)){dline.visible = true;} 
+                            if (cb_obj.active.includes(4)){dline.visible = true;} 
+                             if (cb_obj.active.includes(5)){ddline.visible = true;} 
                                 // 1 index box is bline
-                            if (cb_obj.active.includes(4)){eline.visible = true;} 
+                            if (cb_obj.active.includes(6)){eline.visible = true;} 
                                 // 1 index box is bline
-                            if (cb_obj.active.includes(5)){fline.visible = true;} 
+                            if (cb_obj.active.includes(7)){fline.visible = true;} 
                                 // 1 index box is bline
-                            if (cb_obj.active.includes(6)){gline.visible = true;} 
+                            if (cb_obj.active.includes(8)){gline.visible = true;} 
+                             if (cb_obj.active.includes(9)){ggline.visible = true;} 
                                 // 1 index box is bline
-                            if (cb_obj.active.includes(7)){hline.visible = true;} 
+                            if (cb_obj.active.includes(10)){hline.visible = true;} 
                                 // 1 index box is bline
-                            if (cb_obj.active.includes(8)){iline.visible = true;} 
+                            if (cb_obj.active.includes(11)){iline.visible = true;} 
                                 // 1 index box is bline
-                            if (cb_obj.active.includes(9)){jline.visible = true;} 
+                            if (cb_obj.active.includes(12)){jline.visible = true;} 
                                 // 1 index box is bline
-                            if (cb_obj.active.includes(10)){kline.visible = true;} 
+                            if (cb_obj.active.includes(13)){kline.visible = true;} 
+                             if (cb_obj.active.includes(14)){kkline.visible = true;} 
                                 // 1 index box is bline
                             """,
                             args={'aline': aline, 'bline': bline, 'cline': cline, 'dline': dline,
                                   'eline': eline, 'fline': fline, 'gline': gline, 'hline': hline,
-                                  'iline': iline, 'jline': jline, 'kline': kline})
+                                  'iline': iline, 'jline': jline, 'kline': kline, 'ccline': ccline, 'ddline': ddline,
+                                  'ggline': ggline, 'kkline': kkline})
 checkboxes.js_on_click(callback)
 layout = row(p1, checkboxes)
 # output_file('BV2_DUT_sensors_134_sections.html')
